27671264f102abb8effeccc520c2b63355a45a71
Eveolmedo <evelynolmeedo@gmail.com>
2025-01-25 12:19:21 -0300

    Fix function to add a game

--- backend/models/gamesModel.js
+++ backend/models/gamesModel.js
@@ -14,11 +14,12 @@ const getGameById = (id) => {
 }
 
 const addGame = (newGame) => {
-    const data = getGames()
-    const newId = (data.length + 1).toString()
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    const newId = (data.games.length + 1).toString()
     const game = { ...newGame, id: newId}
 
-    data.push(game)
+    data.games.push(game)
+    data.info.total += 1
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2))
     return game
 }
@@ -27,16 +28,16 @@ const updateGame = (id, updateGame) => {
     const data = getGames()
     const index = data.games.findIndex((game) => game.id === id)
     if (index === -1) return null
-    data[index] = {...data[index], ...updateGame}
+    data.games[index] = {...data.games[index], ...updateGame}
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2))
-    return data[index]
+    return data.games[index]
 }
 
 const deleteGame = (id) => {
     const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
     const index = data.games.findIndex((game) => game.id === id)
     if (index === -1) return false
-    data.splice(index, 1)
+    data.games.splice(index, 1)
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2))
     return true
 }
