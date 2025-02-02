87c0f7e06b72844b1bbeea797547af68d3ac86f4
Eveolmedo <evelynolmeedo@gmail.com>
2025-02-02 18:59:51 -0300

    Test 2

--- backend/index.js
+++ backend/index.js
@@ -12,14 +12,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.use('/api/games', gamesRoutes);
 
 app.use('/', authRoutes);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 // app.use(errorMiddleware)
--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -7,7 +7,7 @@ const LoginForm = () => {
 
   const handleRegister = async () => {
     try {
-      const response = await fetch('https://game-store-api-d2db.onrender.com/register', {
+      const response = await fetch('http://localhost:3000/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -33,7 +33,7 @@ const LoginForm = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('https://game-store-api-d2db.onrender.com/login', {
+      const response = await fetch('http://localhost:3000/login', {
         method: 'POST',
         headers: {'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
