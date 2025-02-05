ee6936907ea09d11455a2388fbfd1d00f2cc7112
Eveolmedo <evelynolmeedo@gmail.com>
2025-02-04 21:37:18 -0300

    Fix Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -6,8 +7,8 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">Game Store</h1>
         <ul className="flex gap-4">
-          <li><a href="/" className="hover:underline">INICIO</a></li>
-          <li><a href="/login" className="hover:underline">INICIAR SESIÓN</a></li>
+          <li><Link to="/" className="hover:underline">INICIO</Link></li>
+          <li><Link to="/login" className="hover:underline">INICIAR SESIÓN</Link></li>
         </ul>
       </div>
     </nav>
